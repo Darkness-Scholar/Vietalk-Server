64017b8d80ae0f856fbf51ae2e12077c4e61fa2f
Hwang <rsoft.hwang99@gmail.com>
2022-06-05 20:03:04 +0700

    fixing deploy

---
+++ root.js
@@ -0,0 +1,8 @@
+global.__mongodb__ = "mongodb+srv://rsoft:123qwe@vietalk.3nlbwj3.mongodb.net/?retryWrites=true&w=majority"
+global.__jwtAccessTokenSecret__ = "vietalk-1802-1999-jats"
+global.__jwtRefreshTokenSecret__ = "vietalk-1802-1999-jrts"
+
+global.__ADMIN_EMAIL_ADDRESS__ = "rsoft.hwang99@gmail.com"
+global.__GOOGLE_CLOUD_CLIENT_ID__ = "174876083930-6kclkg10hs8a6e20en3thvqb0tgmv58f.apps.googleusercontent.com"
+global.__GOOGLE_CLOUD_CLIENT_SECRET__ = "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"
+global.__GOOGLE_CLOUD_CLIENT_REFRESH_TOKEN__ = "1//04WtsHfWTaZ5VCgYIARAAGAQSNwF-L9Irf6m_g9347mc4JkrM6pMejO8FZ39TOCOSfot97O9oXdqzzus5lj0aX2AZ_vAxNSRvL5o"
